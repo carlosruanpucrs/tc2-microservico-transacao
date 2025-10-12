e46f1084031fc9e5792f6ca2497c39674596f6be
Carlos Ruan <c.ruan@edu.pucrs.br>
2025-10-12 12:55:34 -0300

    feat/transacao-extrato: implementado api extrato transacao por conta

--- src/main/java/com/carlosruanpucrs/tc2_microservico_transacao/mapper/TransacaoMapper.java
+++ src/main/java/com/carlosruanpucrs/tc2_microservico_transacao/mapper/TransacaoMapper.java
@@ -1,6 +1,7 @@
 package com.carlosruanpucrs.tc2_microservico_transacao.mapper;
 
 import com.carlosruanpucrs.tc2_microservico_transacao.api.response.ComprovanteResponse;
+import com.carlosruanpucrs.tc2_microservico_transacao.api.response.TransacaoResponse;
 import com.carlosruanpucrs.tc2_microservico_transacao.enums.TipoMovimentacaoEnum;
 import com.carlosruanpucrs.tc2_microservico_transacao.model.entity.TransacaoEntity;
 
@@ -25,6 +26,12 @@ public class TransacaoMapper {
     public static ComprovanteResponse mapToTransferenciaResponse(String comprovante) {
         return ComprovanteResponse.builder()
                 .comprovante(comprovante)
+                .build();
+    }
+
+    public static TransacaoResponse mapToTransacaoResponse(TransacaoEntity transacaoEntity) {
+        return TransacaoResponse.builder()
+
                 .build();
     }
 }
